e2a2a4e1c3d20914b9c546438dc35f4eddc85e91
mdjibo2 <justpourmestest00@gmail.com>
2023-06-16 01:28:35 +0000

    update

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,23 +1,26 @@
-const {describe, it} = require("mocha");
+const { describe, it, beforeEach, afterEach } = require("mocha");
 const sinon = require("sinon");
 const sendPaymentRequestToApi = require("./5-payment");
 const expect = require("chai").expect;
 
-describe("sendPaymentRequestToApi", function() {
-    beforeEach("Set up spy to use for each test", function() {
-	sinon.spy(console, "log");
-    });
-    afterEach("restore spy after each test", function() {
-	console.log.restore();
-    });
-    it("check that console.log is called with the right arg", function() {
-	sendPaymentRequestToApi(100, 20);
+describe("sendPaymentRequestToApi", function () {
+  let consoleLogStub;
 
-	expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
-    });
-    it("check that console.log is called with the right arg", function() {
-	sendPaymentRequestToApi(10, 10);
+  beforeEach(function () {
+    consoleLogStub = sinon.stub(console, "log");
+  });
 
-	expect(console.log.withArgs("The total is: 20").calledOnce).to.be.true;
-    });
+  afterEach(function () {
+    consoleLogStub.restore();
+  });
+
+  it("check that console.log is called with the right arg (test 1)", function () {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleLogStub.withArgs("The total is: 120").calledOnce).to.be.true;
+  });
+
+  it("check that console.log is called with the right arg (test 2)", function () {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleLogStub.withArgs("The total is: 20").calledOnce).to.be.true;
+  });
 });
